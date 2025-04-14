64d56ab9ab7d65deb1d62c19bce8715727088455
Christine <enit.christine@gmail.com>
2025-04-14 13:52:44 +1000

    finished exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let newString='';
+    for (let i=input.length-1; i>-1;i--){
+        newString+=input.slice(i,i+1);
+    }
+    return newString;
 };
 
 // Do not edit below this line
