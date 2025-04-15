0e0bbbd1be932a5c702570703123ddfd6cdcec35
Christine <enit.christine@gmail.com>
2025-04-15 19:34:51 +1000

    completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numStart, numEnd) {
+    let total=0;
+    let buffer=0;
 
+    //check
+    if (!Number.isInteger(numStart)|| !Number.isInteger(numEnd) || numEnd < 0 || numStart<0 ){      
+        return "ERROR";
+    }
+
+    if (numEnd < numStart){
+        buffer = numEnd;
+        numEnd = numStart;
+        numStart = buffer;
+    }
+    
+    for (let i =numStart; i<=numEnd;i++){
+        total+=i;
+    }
+    return total;
 };
 
 // Do not edit below this line
