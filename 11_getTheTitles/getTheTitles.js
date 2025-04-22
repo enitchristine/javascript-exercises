92d8c9e08c932592df4bb43ecf1d7b7f1e9932cb
Christine <enit.christine@gmail.com>
2025-04-22 10:56:24 +1000

    Used map() to solve

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,11 +1,14 @@
 const getTheTitles = function(array) {
-    let titles=[];
-    for (let i=0; i<array.length;i++){
-        titles.push(array[i].title);
-    }
-    console.log(titles);
-    return titles;
+    // let titles=[];
+    // for (let i=0; i<array.length;i++){
+    //     titles.push(array[i].title);
+    // }
+    // console.log(titles);
+    // return titles;
+
+    return array.map((item)=>item.title);
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
