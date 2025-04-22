48e6838c4453a48a6954a1464ca68e65f4877103
Christine <enit.christine@gmail.com>
2025-04-22 11:14:30 +1000

    Used reduce() to solve

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,71 +1,21 @@
 const findTheOldest = function(array) {
 
-    // let oldest;
-    // let prevAge=0;
-    // let curAge=0;
-    // const d = new Date();
-    
-    // oldest = array[0];
-    // if (array[0].yearOfDeath==null){
-    //     prevAge = d.getFullYear()-array[0].yearOfBirth;
-    //     console.log(prevAge);
-    // }else{
-    //     prevAge = array[0].yearOfDeath-array[0].yearOfBirth;
-    // }
-
-    // for (let i=1; i<array.length;i++){
-
-    //     if (array[i].yearOfDeath==null){
-    //         curAge = d.getFullYear();-array[i].yearOfBirth;
-    //         if (curAge>prevAge){
-    //             oldest = array[i];
-    //             prevAge=curAge;
-    //         }
-    //     }else{
-    //         curAge = array[i].yearOfDeath-array[i].yearOfBirth;
-    //         if (curAge>prevAge){
-    //             oldest = array[i];
-    //             prevAge=curAge;
-    //         }
-    //     }
-        
-    // }
-
-    // return oldest;
-
-    // using reduce function
-
     const d = new Date();
-    
 
-    return array.reduce((acc,curVal)=>{
-        let prevAge=0;
-        let curAge=0;
+    return array.reduce((a,b)=>{
+        let aAge;
+        let bAge;
+
+        a.yearOfDeath==null ? aAge=d.getFullYear()-a.yearOfBirth : aAge=a.yearOfDeath-a.yearOfBirth;
+        b.yearOfDeath==null ? bAge=d.getFullYear()-b.yearOfBirth : bAge=b.yearOfDeath-b.yearOfBirth;
         
-        console.log(acc.name+acc.yearOfDeath);
-        if (acc.yearOfDeath === undefined){
-            
-            prevAge=d.getFullYear()-acc.yearOfBirth;  
-            console.log(prevAge); 
+        if (aAge>bAge){
+            return a;
+        }else {
+            return b;
         }
-        else{
-            prevAge=acc.yearOfDeath-acc.yearOfBirth;
-
-
-        }if (curVal.yearOfDeath===undefined){
-            curAge=d.getFullYear()-curVal.yearOfBirth;
-        }
-        else{
-            curAge=curVal.yearOfDeath-curVal.yearOfBirth;
-        }
-
-        console.log(acc.name+prevAge);
-        if (prevAge>curAge){
-            return acc;
-        }else{
-            return curVal;
-        }
-    } ,array[0] );
+    
+    })
 };
 
 // Do not edit below this line
